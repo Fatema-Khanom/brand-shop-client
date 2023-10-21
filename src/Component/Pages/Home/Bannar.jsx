54b5c291c2f4c44765bbe6f8c535ec88ac892a2d
Fatema Khanom <fatemaurmi2019@gmail.com>
2023-10-21 21:30:32 +0600

    Fixed active route problem

--- src/Component/Pages/Home/Bannar.jsx
+++ src/Component/Pages/Home/Bannar.jsx
@@ -5,7 +5,7 @@ const Bannar = () => {
        
 
         <div>
-        <div className="relative">
+        {/* <div className="relative">
         <img src="https://i.ibb.co/4SrJGfj/240-F-615699215-b-LR3bht-Z5w7ak-Rv-Grfq-Ta-Tf-Mt-ILKG0a-V.jpg" alt="Car Banner" className="w-full h-auto" />
         <div className="absolute inset-0 flex flex-col md:-mt-60  justify-center text-white">
             <h1 className=" pt-4 text-2xl md:text-6xl font-extrabold md:  text-center">Discover Excellence in Every Ride</h1>
@@ -13,7 +13,7 @@ const Bannar = () => {
             At our car brand shop, we redefine the driving experience. Each vehicle in our collection is a masterpiece, designed with precision and passion. From cutting-edge technology to unparalleled comfort, we ensure excellence in every detail. Explore our range and embark on a journey where performance meets elegance.
             </p>
         </div>
-        </div>
+        </div> */}
         </div>
 
 
--- src/Component/Pages/Home/ContactUs.jsx
+++ src/Component/Pages/Home/ContactUs.jsx
@@ -1,20 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import React from 'react';
 
 const ContactUs = () => {
   const backgroundImage = "url('https://i.ibb.co/ysytt0x/raphael-maksian-Qn7b-Jt56s-GY-unsplash.jpg')";
-  const [isAnimated, setIsAnimated] = useState(false);
-
-  useEffect(() => {
-   
-    const animationTimeout = setTimeout(() => {
-      setIsAnimated(true);
-    }, 500);
-
-    return () => {
-      clearTimeout(animationTimeout);
-    };
-  }, []);
 
   return (
     <div
@@ -24,72 +11,52 @@ const ContactUs = () => {
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold text-white mb-6">Contact Us</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <CSSTransition
-            in={isAnimated}
-            timeout={500}
-            classNames={{
-              enter: 'opacity-0',
-              enterActive: 'opacity-100',
-            }}
-            unmountOnExit
-          >
-            <div className="bg-opacity-0 rounded-lg p-6 backdrop-blur-lg">
-              <h3 className="text-2xl font-semibold mb-4">Contact Information</h3>
-              <ul className="text-lg">
-                <li>
-                  <span className="text-gray-300">Address:</span> 123 Main Street, City, Country
-                </li>
-                <li>
-                  <span className="text-gray-300">Email:</span> contact@example.com
-                </li>
-                <li>
-                  <span className="text-gray-300">Phone:</span> +1 (123) 456-7890
-                </li>
-              </ul>
-            </div>
-          </CSSTransition>
-          <CSSTransition
-            in={isAnimated}
-            timeout={500}
-            classNames={{
-              enter: 'opacity-0',
-              enterActive: 'opacity-100',
-            }}
-            unmountOnExit
-          >
-            <div className="bg-opacity-80 rounded-lg p-6 backdrop-blur-lg">
-              <h3 className="text-2xl font-semibold mb-4">Send us a Message</h3>
-              <form>
-                <div className="mb-4">
-                  <input
-                    type="text"
-                    className="w-full py-2 px-3 text-gray-900 bg-gray-100 rounded-md focus:outline-none focus:shadow-outline"
-                    placeholder="Your Name"
-                  />
-                </div>
-                <div className="mb-4">
-                  <input
-                    type="email"
-                    className="w-full py-2 px-3 text-gray-900 bg-gray-100 rounded-md focus:outline-none focus:shadow-outline"
-                    placeholder="Your Email"
-                  />
-                </div>
-                <div className="mb-4">
-                  <textarea
-                    rows="4"
-                    className="w-full py-2 px-3 text-gray-900 bg-gray-100 rounded-md focus:outline-none focus:shadow-outline"
-                    placeholder="Your Message"
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="bg-white hover:bg-gray-300 text-red-700 font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
-                >
-                  Send Message
-                </button>
-              </form>
-            </div>
-          </CSSTransition>
+          <div className="bg-opacity-0 rounded-lg p-6 backdrop-blur-lg">
+            <h3 className="text-2xl font-semibold mb-4">Contact Information</h3>
+            <ul className="text-lg">
+              <li>
+                <span className="text-gray-300">Address:</span> 123 Main Street, City, Country
+              </li>
+              <li>
+                <span className="text-gray-300">Email:</span> contact@example.com
+              </li>
+              <li>
+                <span className="text-gray-300">Phone:</span> +1 (123) 456-7890
+              </li>
+            </ul>
+          </div>
+          <div className="bg-opacity-80 rounded-lg p-6 backdrop-blur-lg">
+            <h3 className="text-2xl font-semibold mb-4">Send us a Message</h3>
+            <form>
+              <div className="mb-4">
+                <input
+                  type="text"
+                  className="w-full py-2 px-3 text-gray-900 bg-gray-100 rounded-md focus:outline-none focus:shadow-outline"
+                  placeholder="Your Name"
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  type="email"
+                  className="w-full py-2 px-3 text-gray-900 bg-gray-100 rounded-md focus:outline-none focus:shadow-outline"
+                  placeholder="Your Email"
+                />
+              </div>
+              <div className="mb-4">
+                <textarea
+                  rows="4"
+                  className="w-full py-2 px-3 text-gray-900 bg-gray-100 rounded-md focus:outline-none focus:shadow-outline"
+                  placeholder="Your Message"
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-white hover-bg-gray-300 text-red-700 font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
+              >
+                Send Message
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
